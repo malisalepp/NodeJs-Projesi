03353548c1268accf096b6be1a4d9abcbfe0df38
malisalepp <mehmetalisalep1438@gmail.com>
2025-07-15 22:48:19 +0300

    NodeJs Projesi
    Erasmus'da yaptığım NodeJs ödevim

---
+++ node-webserver/node-webserver/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// Catch-all route for any other paths
+app.get('*', (req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
